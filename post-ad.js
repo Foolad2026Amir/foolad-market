11fae69d8eb1514ba6b63f81bb658fb90cb2ba7f
Foolad2026Amir <amiralimajidi5561@gmail.com>
2026-07-14 00:50:39 -0700

    Update post-ad.js

--- post-ad.js
+++ post-ad.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const nextButton = document.querySelector(".next-btn");
+
+    nextButton.addEventListener("click", function () {
+
+        alert("مرحله دوم با موفقیت تکمیل شد ✅\n\nمرحله سوم (آپلود تصاویر) به زودی نمایش داده می‌شود.");
+
+    });
+
+});
